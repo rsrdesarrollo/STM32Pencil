001d5f50d851109d0c758811e1604ac6610cef4f
Raúl Sampedro <rsrdesarrollo@gmail.com>
2014-04-08 00:20:23 +0200

    initial

---
+++ Makefile
@@ -0,0 +1,85 @@
+
+TARGET:=Pencil
+TOOLCHAIN_PATH:=/opt/arm/gcc-arm-none-eabi-4_8-2014q1/bin
+TOOLCHAIN_PREFIX:=arm-none-eabi
+OPTLVL:=3 # Optimization level, can be [0, 1, 2, 3, s].
+
+PROJECT_NAME:=$(notdir $(lastword $(CURDIR)))
+TOP:=/usr/share/stm32vldiscovery/an3268/stm32vldiscovery_package
+DISCOVERY:=$(TOP)/Utilities
+STMLIB:=$(TOP)/Libraries
+STD_PERIPH:=$(STMLIB)/STM32F10x_StdPeriph_Driver
+CORE:=$(STMLIB)/CMSIS/CM3/CoreSupport
+DEVICE:=$(STMLIB)/CMSIS/CM3/DeviceSupport/ST/STM32F10x
+STARTUP:=$(STMLIB)/CMSIS/CM3/DeviceSupport/ST/STM32F10x/startup/gcc_ride7
+LINKER_SCRIPT:="$(CURDIR)/STM32F100RB_FLASH.ld"
+
+INCLUDE=-I"$(CURDIR)"
+INCLUDE+=-I"$(CORE)"
+INCLUDE+=-I"$(DEVICE)"
+INCLUDE+=-I"$(STD_PERIPH)/inc"
+INCLUDE+=-I"$(DISCOVERY)"
+
+# vpath is used so object files are written to the current directory instead
+# of the same directory as their source files
+vpath %.c $(STD_PERIPH)/src $(DEVICE) $(CORE) $(DISCOVERY) 
+vpath %.s $(STARTUP)
+
+ASRC=startup_stm32f10x_md_vl.s
+
+# Project Source Files
+SRC=main.c
+SRC+=stm32f10x_it.c
+
+#CMSIS
+SRC+=system_stm32f10x.c
+
+# Discovery Source Files
+SRC+=STM32vldiscovery.c
+
+# Standard Peripheral Source Files
+SRC+=misc.c
+SRC+=stm32f10x_exti.c
+SRC+=stm32f10x_gpio.c
+SRC+=stm32f10x_rcc.c
+
+CDEFS=-DSTM32F10X_MD_VL
+CDEFS+=-DUSE_STDPERIPH_DRIVER
+
+MCUFLAGS=-mcpu=cortex-m3 -mthumb
+COMMONFLAGS=-O$(OPTLVL) -g -Wall -Werror 
+CFLAGS=$(COMMONFLAGS) $(MCUFLAGS) $(INCLUDE) $(CDEFS)
+
+LDLIBS=
+LDFLAGS=$(COMMONFLAGS) -fno-exceptions -ffunction-sections -fdata-sections \
+        -nostartfiles -Wl,--gc-sections,-T$(LINKER_SCRIPT)
+
+#####
+#####
+
+OBJ = $(SRC:%.c=%.o) $(ASRC:%.s=%.o)
+
+CC=$(TOOLCHAIN_PATH)/$(TOOLCHAIN_PREFIX)-gcc
+LD=$(TOOLCHAIN_PATH)/$(TOOLCHAIN_PREFIX)-gcc
+OBJCOPY=$(TOOLCHAIN_PATH)/$(TOOLCHAIN_PREFIX)-objcopy
+AS=$(TOOLCHAIN_PATH)/$(TOOLCHAIN_PREFIX)-as
+AR=$(TOOLCHAIN_PATH)/$(TOOLCHAIN_PREFIX)-ar
+GDB=$(TOOLCHAIN_PATH)/$(TOOLCHAIN_PREFIX)-gdb
+
+
+all: STM32F100RB_FLASH.ld $(OBJ)
+	$(CC) -o $(TARGET).elf $(LDFLAGS) $(OBJ)	$(LDLIBS)
+	$(OBJCOPY) -O ihex   $(TARGET).elf $(TARGET).hex
+	$(OBJCOPY) -O binary $(TARGET).elf $(TARGET).bin
+
+STM32F100RB_FLASH.ld:
+	@echo "Please copy STM32F100RB_FLASH.ld to the current directory."
+	@false
+
+.PHONY: clean
+
+clean:
+	rm -f $(OBJ)
+	rm -f $(TARGET).elf
+	rm -f $(TARGET).hex
+	rm -f $(TARGET).bin
---
+++ main.c
@@ -0,0 +1,145 @@
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x.h"
+#include "STM32vldiscovery.h"
+
+NVIC_InitTypeDef NVIC_InitStructure;
+
+void setSavePowerMode();
+void initPintOutput(GPIO_TypeDef * GPIOX, uint16_t pin);
+void configureIRSensorInt();
+void configureUltrasonicSensorInt();
+
+int main(void) {
+	/*!< At this stage the microcontroller clock setting is already configured,
+	 this is done through SystemInit() function which is called from startup
+	 file (startup_stm32f10x_xx.s) before to branch to application main.
+	 To reconfigure the default setting of SystemInit() function, refer to
+	 system_stm32f10x.c file
+	 */
+
+	/* Initialize LD3 and USER Button mounted on STM32VLDISCOVERY board*/
+	setSavePowerMode();
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOC, ENABLE);
+	initPintOutput(GPIOC, GPIO_Pin_8 | GPIO_Pin_9);
+	configureIRSensorInt();
+
+	STM32vldiscovery_LEDOn(LED3);
+
+	while (1) {
+	}
+}
+
+#ifdef  USE_FULL_ASSERT
+
+void assert_failed(uint8_t* file, uint32_t line)
+{
+	while (1);
+}
+#endif
+
+void setSavePowerMode() {
+
+	/* Set al the pins on Analog input mode and unable Clock.
+	 * */
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |
+	RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD |
+	RCC_APB2Periph_GPIOE, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_All;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_Init(GPIOC, &GPIO_InitStructure);
+	GPIO_Init(GPIOD, &GPIO_InitStructure);
+	GPIO_Init(GPIOE, &GPIO_InitStructure);
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |
+	RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOD |
+	RCC_APB2Periph_GPIOE, DISABLE);
+}
+
+void initPintOutput(GPIO_TypeDef * GPIOX, uint16_t pin) {
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	GPIO_StructInit(&GPIO_InitStructure);
+	GPIO_InitStructure.GPIO_Pin = pin;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOX, &GPIO_InitStructure);
+}
+
+void configureIRSensorInt() {
+	GPIO_InitTypeDef GPIO_InitStructure;
+	EXTI_InitTypeDef EXTI_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	/* Enable the IRSensor Clock */
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+
+	/* Configure IRSensor pin as input floating */
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+	/* Connect IRSensor EXTI Line to Button GPIO Pin */
+	GPIO_EXTILineConfig(GPIO_PortSourceGPIOA, GPIO_PinSource0);
+
+	/* Configure IRSensor EXTI line */
+	EXTI_InitStructure.EXTI_Line = EXTI_Line0;
+	EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+
+	EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Falling;
+
+	EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+	EXTI_Init(&EXTI_InitStructure);
+
+	/* Enable and set IRSensor EXTI Interrupt to the lowest priority */
+	NVIC_InitStructure.NVIC_IRQChannel = EXTI0_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0F;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0F;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+
+	NVIC_Init(&NVIC_InitStructure);
+
+}
+
+void configureUltrasonicSensorInt(){
+//	GPIO_InitTypeDef GPIO_InitStructure;
+//	EXTI_InitTypeDef EXTI_InitStructure;
+//	NVIC_InitTypeDef NVIC_InitStructure;
+//
+//	/* Enable the IRSensor Clock */
+//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO, ENABLE);
+//
+//	/* Configure IRSensor pin as input floating */
+//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+//	GPIO_Init(GPIOB, &GPIO_InitStructure);
+//
+//	/* Connect IRSensor EXTI Line to Button GPIO Pin */
+//	GPIO_EXTILineConfig(GPIO_PortSourceGPIOB, GPIO_PinSource0);
+//
+//	/* Configure IRSensor EXTI line */
+//	EXTI_InitStructure.EXTI_Line = EXTI_Line1;
+//	EXTI_InitStructure.EXTI_Mode = EXTI_Mode_Interrupt;
+//
+//	EXTI_InitStructure.EXTI_Trigger = EXTI_Trigger_Rising;
+//
+//	EXTI_InitStructure.EXTI_LineCmd = ENABLE;
+//	EXTI_Init(&EXTI_InitStructure);
+//
+//	/* Enable and set IRSensor EXTI Interrupt to the lowest priority */
+//	NVIC_InitStructure.NVIC_IRQChannel = EXTI1_IRQn;
+//	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0F;
+//	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x0F;
+//	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+//
+//	NVIC_Init(&NVIC_InitStructure);
+}
+
+
